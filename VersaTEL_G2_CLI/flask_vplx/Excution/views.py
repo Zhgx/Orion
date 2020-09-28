fe3bd51b12974ef38aee8c420d3476e0aad377e3
Paul.Wen <Paul.Wen@feixitek.com>
2020-09-28 15:56:27 +0800

    test

--- VersaTEL_G2_CLI/flask_vplx/Excution/views.py
+++ VersaTEL_G2_CLI/flask_vplx/Excution/views.py
@@ -4,4 +4,4 @@ from flask import views
 from flask_vplx.Excution import excution_blue
 from flask_vplx.Excution import model
 
-excution_blue.add_url_rule('/iscsi_map', view_func=model.iscsiMapView.as_view('map_view'))
+# excution_blue.add_url_rule('/iscsi_map', view_func=model.iscsiMapView.as_view('map_view'))
