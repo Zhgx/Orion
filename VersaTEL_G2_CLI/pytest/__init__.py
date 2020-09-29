32462e96f88416903780e8a7789e8c2495db79a4
zane <zane.chen@feixitek.com>
2020-09-29 13:43:50 +0800

    add pytest/__init__.py pytest/pytest.ini pytest/test_iscsi.py

---
+++ VersaTEL_G2_CLI/pytest/__init__.py
@@ -0,0 +1,18 @@
+import consts
+import sys
+import log
+import sundry as s
+
+# sys.path.append('../')
+# consts._init()
+#
+# transaction_id = s.create_transaction_id()
+# logger = log.Log('pytest',transaction_id)
+# consts.set_glo_log(logger)
+# consts.set_glo_tsc_id(transaction_id)
+# # consts.set_glo_id(99)
+# # consts.set_glo_str('pytest')
+# consts.set_glo_rpl('no')
+
+
+consts.set_glo_rpl('no')
---
+++ VersaTEL_G2_CLI/pytest/test_iscsi.py
@@ -0,0 +1,43 @@
+import iscsi from execute
+
+
+class TestDisk:
+
+    def setup_class(self):
+        self.disk = iscsi.Disk()
+
+    def test_get_all_disk(self):
+        assert self.disk.get_all_disk() == None
+
+    def test_get_spe_disk(self):
+        assert self.disk.get_spe_disk('res_a') == None
+
+    def test_show_all_disk(self):
+        assert self.disk.show_all_disk() == None
+
+    def test_show_spe_disk(self):
+        assert self.disk.show_spe_disk('res_a') == None
+
+
+class TestHost:
+
+    def setup_class(self):
+        self.host = iscsi.Host()
+
+    def test_create_host(self):
+        assert self.host.create_host() == None
+
+    def test_get_all_host(self):
+        assert self.host.get_all_host() == None
+
+    def test_get_spe_host(self):
+        assert self.host.get_spe_host() == None
+
+    def test_show_all_host(self):
+        assert self.host.show_all_host() == None
+
+    def test_show_spe_host(self):
+        assert self.host.show_spe_host() == None
+
+    def test_delete_host(self):
+        assert self.host.delete_host() == None
